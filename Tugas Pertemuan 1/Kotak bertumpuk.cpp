562f200d7118a86bcf7c9e398e44fdf6db5dcd6d
kamal hamzah <kamalhamzah.sw@gmail.com>
2026-03-13 08:42:24 +0700

    Pertemuan 2 check

--- Tugas Pertemuan 1/Kotak bertumpuk.cpp
+++ Tugas Pertemuan 1/Kotak bertumpuk.cpp
@@ -5,35 +5,30 @@ void display(void) {
 
     glBegin(GL_QUADS);
     
-    // Baris Bawah - Kubus 1 (Merah)
     glColor3f(1.0f, 0.0f, 0.0f);
     glVertex2f(-0.55f, -0.6f);
     glVertex2f(-0.25f, -0.6f);
     glVertex2f(-0.25f, -0.3f);
     glVertex2f(-0.55f, -0.3f);
 
-    // Baris Bawah - Kubus 2 (Hijau)
     glColor3f(0.0f, 1.0f, 0.0f);
     glVertex2f(-0.15f, -0.6f);
     glVertex2f(0.15f, -0.6f);
     glVertex2f(0.15f, -0.3f);
     glVertex2f(-0.15f, -0.3f);
 
-    // Baris Bawah - Kubus 3 (Kuning)
     glColor3f(1.0f, 1.0f, 0.0f);
     glVertex2f(0.25f, -0.6f);
     glVertex2f(0.55f, -0.6f);
     glVertex2f(0.55f, -0.3f);
     glVertex2f(0.25f, -0.3f);
 
-    // Baris Atas - Kubus 4 (Magenta)
     glColor3f(1.0f, 0.0f, 1.0f);
     glVertex2f(-0.35f, -0.25f);
     glVertex2f(-0.05f, -0.25f);
     glVertex2f(-0.05f, 0.05f);
     glVertex2f(-0.35f, 0.05f);
-
-    // Baris Atas - Kubus 5 (Cyan)
+    
     glColor3f(0.0f, 1.0f, 1.0f);
     glVertex2f(0.05f, -0.25f);
     glVertex2f(0.35f, -0.25f);
